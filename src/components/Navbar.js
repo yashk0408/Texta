010bef8eb516ea0bf77432ece4ee4d5ba1134539
yashk0408 <yashk2824@gmail.com>
2022-07-28 20:45:12 +0530

    href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,8 @@ export default function Navbar(props) {
   const capitalize = (word)=>{
     const lower = word.toLowerCase();
     return lower.charAt(0).toUpperCase() + lower.slice(1);
-}
+   }
+
   return (
     // <nav className={`navbar navbar-expand-lg navbar-dark bg-dark`}>
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
@@ -19,7 +20,8 @@ export default function Navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
+            <a className="nav-link active" aria-current="page" href="https://yashk0408.github.io/Texta/">Home</a>
+            
           </li>
           <li className="nav-item">
             {/* <a className="nav-link" href="/">about</a> */}
